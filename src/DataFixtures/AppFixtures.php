cb03c72493089f84a9775c7563e66b59e9a31464
Raspgot <gauthierw630@gmail.com>
2019-12-27 13:40:10 +0100

    Slugify uri

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,26 +3,29 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
-use Doctrine\Bundle\FixturesBundle\Fixture;
-Use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Cocur\Slugify\Slugify;
+Use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('FR-fr');
+        $slugify = new Slugify();
 
         for ($i=1; $i<=30 ; $i++) {
             $ad = new Ad();
 
             $title = $faker->sentence();
+            $slug = $slugify->slugify($title);
             $coverImage = $faker->imageUrl(1000,350);
             $intro = $faker->paragraph(2);
             $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
 
             $ad ->setTitle($title)
-                ->setSlug("titre-annonce-$i")
+                ->setSlug($slug)
                 ->setCoverImage($coverImage)
                 ->setIntroduction($intro)
                 ->setContent($content)
